4d4b9841ee809295413c910c4cc12307685b2714
Indunil1992 <indunil@adroitlogic.com>
2020-04-09 10:51:54 +0530

    1

--- MyFirstProject.js
+++ MyFirstProject.js
@@ -1,4 +1,17 @@
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    debugger;
+    try {
+        let data = await s3.listObjects({
+            Bucket: "cf-templates-1oea0dbk84gyv-us-west-2",
+            MaxKeys: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed 1" };
 };
